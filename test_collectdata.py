f4adc7d4a343d719605277bf7644eed90e212a9d
yz398 <35477789+yz398@users.noreply.github.com>
2018-03-02 01:14:53 -0500

    Update test_collectdata.py

--- test_collectdata.py
+++ test_collectdata.py
@@ -7,6 +7,6 @@ def test_collectdata():
         from list_module.collectdata import collectdata
     except ImportError:
         print("Necessary imports for this test function failed")
-        return 
+        return
     with pytest.raises(TypeError):
         collectdata([1, 2, 3])
